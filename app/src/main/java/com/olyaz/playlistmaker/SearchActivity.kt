cb72f575b758405bebce9be3d4913f9dea77a105
Olga Zorina <zorinavlolga@gmail.com>
2024-09-18 11:27:56 +0300

    11 sprint, improvements

--- app/src/main/java/com/olyaz/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/olyaz/playlistmaker/SearchActivity.kt
@@ -1,19 +1,13 @@
 package com.olyaz.playlistmaker
 
-import android.content.Context
+
 import android.os.Bundle
-import android.view.View
 import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
-import android.widget.Button
-import android.widget.EditText
-import android.widget.ImageView
-import android.widget.LinearLayout
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.getSystemService
 import androidx.core.view.isVisible
 import androidx.core.widget.addTextChangedListener
-import androidx.recyclerview.widget.RecyclerView
 import com.olyaz.playlistmaker.databinding.ActivitySearchBinding
 import retrofit2.Call
 import retrofit2.Callback
@@ -27,12 +21,7 @@ class SearchActivity : AppCompatActivity() {
 
     private var searchStringValue: String? = null
     private lateinit var binding: ActivitySearchBinding
-    private lateinit var searchEditText: EditText
-    private lateinit var searchRecyclerView: RecyclerView
-    private lateinit var placeholderMessageView: LinearLayout
-    private lateinit var placeholderImage: ImageView
-    private lateinit var placeholderText: TextView
-    private lateinit var placeholderButton: Button
+
 
     private val iTunesSearchBaseUrl= "https://itunes.apple.com"
     private val retrofit = Retrofit.Builder()
@@ -48,52 +37,46 @@ class SearchActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivitySearchBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        searchEditText = binding.searchEditText
-        searchRecyclerView = binding.searchRecyclerView
-        placeholderMessageView = binding.placeholderMessage
-        placeholderImage = binding.placeholderImage
-        placeholderText = binding.placeholderText
-        placeholderButton = binding.placeholderButton
 
         trackAdapter.tracks = listOfTracks
-        searchRecyclerView.adapter = trackAdapter
+        binding.searchRecyclerView.adapter = trackAdapter
 
         binding.searchToolbar.setNavigationOnClickListener {
             finish()
         }
 
         if (savedInstanceState != null) {
-            searchEditText.setText(searchStringValue)
+            binding.searchEditText.setText(searchStringValue)
             binding.clearButton.isVisible = !binding.searchEditText.text.isNullOrEmpty()
         }
 
         binding.clearButton.setOnClickListener {
             binding.searchEditText.text.clear()
-            binding.clearButton.visibility = View.INVISIBLE
-            val inputManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-            inputManager.hideSoftInputFromWindow(binding.searchEditText.windowToken, 0)
-            searchRecyclerView.visibility = View.GONE
-            placeholderMessageView.visibility = View.GONE
+            binding.clearButton.hide()
+            val inputManager = getSystemService<InputMethodManager>()
+            inputManager?.hideSoftInputFromWindow(binding.searchEditText.windowToken, 0)
+            binding.searchRecyclerView.gone()
+            binding.placeholderMessage.gone()
             listOfTracks.clear()
             trackAdapter.notifyDataSetChanged()
         }
 
-        searchEditText.addTextChangedListener(
+        binding.searchEditText.addTextChangedListener(
             onTextChanged = {s: CharSequence?, start: Int, before: Int, count: Int ->
                 searchStringValue = s.toString()
                 binding.clearButton.isVisible = !searchStringValue.isNullOrEmpty()
             }
         )
 
-        searchEditText.setOnEditorActionListener { _, actionId, _ ->
+        binding.searchEditText.setOnEditorActionListener { _, actionId, _ ->
             if (actionId == EditorInfo.IME_ACTION_DONE) {
                 searchSongs()
-                true
+                return@setOnEditorActionListener true
             }
             false
         }
 
-        placeholderButton.setOnClickListener {
+        binding.placeholderButton.setOnClickListener {
             searchSongs()
         }
     }
@@ -101,7 +84,6 @@ class SearchActivity : AppCompatActivity() {
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
         outState.putString(SEARCH_VALUE_KEY,searchStringValue)
-
     }
 
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
@@ -110,15 +92,15 @@ class SearchActivity : AppCompatActivity() {
     }
 
     private fun searchSongs() {
-        if (searchEditText.text.isNotEmpty()) {
+        if (binding.searchEditText.text.isNotEmpty()) {
             iTunesSearchService.search(searchStringValue!!).enqueue(object : Callback<TrackResponse>{
                 override fun onResponse(
                     call: Call<TrackResponse>,
                     response: Response<TrackResponse>
                 ) {
                     if (response.code() == 200) {
-                        placeholderMessageView.visibility = View.GONE
-                        searchRecyclerView.visibility = View.VISIBLE
+                        binding.placeholderMessage.gone()
+                        binding.searchRecyclerView.show()
                         listOfTracks.clear()
                         if (response.body()?.results?.isNotEmpty() == true) {
                             listOfTracks.addAll(response.body()?.results!!)
@@ -141,19 +123,19 @@ class SearchActivity : AppCompatActivity() {
     }
 
     private fun showMessage(reason: SearchError) {
-        searchRecyclerView.visibility = View.GONE
-        placeholderMessageView.visibility = View.VISIBLE
+        binding.searchRecyclerView.gone()
+        binding.placeholderMessage.show()
         when (reason) {
             SearchError.EMPTY -> {
-                placeholderText.text = getString(R.string.nothing_found)
-                placeholderButton.visibility = View.GONE
-                placeholderImage.setImageResource(R.drawable.empty_search)
+                binding.placeholderText.text = getString(R.string.nothing_found)
+                binding.placeholderButton.gone()
+                binding.placeholderImage.setImageResource(R.drawable.empty_search)
             }
 
             SearchError.CONNECTION -> {
-                placeholderText.text = getString(R.string.connection_error)
-                placeholderButton.visibility = View.VISIBLE
-                placeholderImage.setImageResource(R.drawable.connection_error)
+                binding.placeholderText.text = getString(R.string.connection_error)
+                binding.placeholderButton.show()
+                binding.placeholderImage.setImageResource(R.drawable.connection_error)
             }
         }
     }
